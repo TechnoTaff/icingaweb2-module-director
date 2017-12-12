ee749a0d4f4f4fcd133424b98ab50519ea094c00
root <root@plxuimon006.plxucloud.net>
2017-12-12 14:01:08 +0000

    Added 'Services' CLI

---
+++ application/clicommands/ServicesCommand.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Icinga\Module\Director\Clicommands;
+
+use Icinga\Module\Director\Cli\ObjectsCommand;
+
+/**
+ * Manage Icinga Services
+ *
+ * Use this command to list Icinga Service objects
+ */
+class ServicesCommand extends ObjectsCommand
+{
+}
